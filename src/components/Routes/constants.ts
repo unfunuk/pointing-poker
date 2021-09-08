21973bee57e80da9179c21abb9a24aa7cc3db5c7
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-08 23:54:03 +0300

    fix:return component MAIN in router

--- src/components/Routes/constants.ts
+++ src/components/Routes/constants.ts
@@ -3,13 +3,12 @@ import Game from "../../pages/Game";
 import Lobby from "../../pages/Lobby";
 import Main from "../../pages/Main";
 import Settings from "../../pages/Settings";
-import Input from "../Input/Input";
 
 const ROUTES: Route[] = [
   {
     path: "/",
     key: "main",
-    component: Input,
+    component: Main,
   },
   {
     path: "/settings",
