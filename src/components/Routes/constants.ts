048f59b78f13a5170d64345dfb19df725d08def0
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-08 22:11:06 +0300

    feat: implement input without styles

--- src/components/Routes/constants.ts
+++ src/components/Routes/constants.ts
@@ -3,12 +3,13 @@ import Game from "../../pages/Game";
 import Lobby from "../../pages/Lobby";
 import Main from "../../pages/Main";
 import Settings from "../../pages/Settings";
+import Input from "../Input/Input";
 
 const ROUTES: Route[] = [
   {
     path: "/",
     key: "main",
-    component: Main,
+    component: Input,
   },
   {
     path: "/settings",
