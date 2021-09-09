8f20315cf53de03385a416ea45e1f2ca7f86f4e2
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-09 22:15:43 +0300

    fix: make changes according to the comments

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,30 +1,26 @@
 import React, { useState } from "react";
-import classNames from "classnames";
+
 import "./input.scss";
 
 function Input(): JSX.Element {
-  const [url, setUrl] = useState("");
+  const [value, setValue] = useState("");
 
   return (
-    <form className="form-url">
-      <p className="form-url__text">
-        Connect to lobby by <span className="form-url__text_span">URL</span>:
-      </p>
-      <div className="form-url__div">
-        <label>
-          <input
-            className="form-url__div_input"
-            type="text"
-            name="input-url"
-            value={url}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setUrl(event.target.value);
-            }}
-          />
-        </label>
-        <input type="submit" value="Connect" className="form-url__div_submit" />
-      </div>
-    </form>
+    <div className="input">
+      <label>
+        <p className="input__description">Label:</p>
+        <input
+          className="input__field"
+          type="text"
+          name="input"
+          value={value}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setValue(event.target.value);
+          }}
+        />
+      </label>
+      {/* TO DO: add Button component after its merge */}
+    </div>
   );
 }
 
