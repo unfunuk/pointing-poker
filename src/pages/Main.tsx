29bdb344d4208f5aed6e604d724b53876aa0955a
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-15 23:25:45 +0300

    fix: remove Static from Main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,12 +3,7 @@ import Header from "../components/Header/Header";
 import Static from "../components/Static/Static";
 
 function Main(): JSX.Element {
-  return (
-    <div>
-      <Header />
-      <Static />
-    </div>
-  );
+  return <div>Main</div>;
 }
 
 export default Main;
