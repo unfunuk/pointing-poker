60119df59e17a745c53bb4d00fa29566f88b8beb
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-15 23:26:48 +0300

    refactor: delete comments

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Static from "../components/Static/Static";
 
 function Main(): JSX.Element {
   return <div>Main</div>;
