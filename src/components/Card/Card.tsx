d6444669ac63c69725137cde44c7bff1e32325f1
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-19 22:29:57 +0300

    feaf: provide an opportunity to put images as content in Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,10 +3,16 @@ import "./card.scss";
 import { CardValue } from "./types";
 
 function Card({ value, content }: CardValue): JSX.Element {
+  const regExp = /(?:jpg|jpeg|png|ico)$/;
+
   return (
     <div className="card">
       <p className="card__top">{value}</p>
-      <p className="card__content">{content}</p>
+      {regExp.test(content) ? (
+        <img src={content} alt={content} className="card__content_img" />
+      ) : (
+        <p className="card__content">{content}</p>
+      )}
       <p className="card__bottom">{value}</p>
     </div>
   );
