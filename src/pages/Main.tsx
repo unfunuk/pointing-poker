02e22966b13f0a879cd7e5b9a35e042b73829b10
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-19 20:38:59 +0300

    fix: make changes according comments

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button/Button";
+import { Buttons } from "../components/Button/constants";
+import Input from "../components/Input/Input";
+import "./main.scss";
 
 function Main(): JSX.Element {
-  return <div>Main</div>;
+  const [url, setUrl] = useState("");
+  return (
+    <div className="static">
+      <div className="static__title">
+        <span>
+          <img src="cards.jpg" alt="image of the cards" />
+        </span>
+        <span className="static__title_text static__title_poker">Poker</span>
+        <p className="static__title_slash"></p>
+        <span className="static__title_text static__title_planning">
+          Plannig
+        </span>
+      </div>
+      <div className="static__section">
+        <p className="static__section_text">Start your planning:</p>
+        <div className="static__section_button">
+          <p className="static__section_discription">Create session:</p>
+          <Button type={Buttons.Primary} text="New game" />
+        </div>
+      </div>
+      <div className="static__section">
+        <p className="static__section_text">OR:</p>
+        <Input
+          label="Connect to lobby by URL:"
+          Button={<Button type={Buttons.Primary} text="Connect" />}
+          setUpperValue={setUrl}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Main;
