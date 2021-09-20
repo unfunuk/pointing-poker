c417e552918f14c8149d311186fe80e6feaec9e4
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-20 21:32:13 +0300

    refactor: make code cleanly

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -6,14 +6,11 @@ function Dropdown({ options }: Dropdownopions): JSX.Element {
   return (
     <form>
       <select className="select">
-        {options.map((option) => {
-          return (
+      {options.map((option) => 
             <option className="select__option" key={option}>
               {option}
             </option>
-          );
-        })}
-        {console.log(options)}
+        )}
       </select>
     </form>
   );
