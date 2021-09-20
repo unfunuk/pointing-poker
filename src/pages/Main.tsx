964391dcaffe864faf2b5d31e15b39e0bc98614a
Gleb Baykov <gleb.baykov99@gmail.com>
2021-09-20 22:02:51 +0300

    refactor: format SCSS anf Main files

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,26 +7,24 @@ import "./main.scss";
 function Main(): JSX.Element {
   const [url, setUrl] = useState("");
   return (
-    <div className="static">
-      <div className="static__title">
+    <div className="main">
+      <div className="main__title">
         <span>
           <img src="cards.jpg" alt="image of the cards" />
         </span>
-        <span className="static__title_text static__title_poker">Poker</span>
-        <p className="static__title_slash"></p>
-        <span className="static__title_text static__title_planning">
-          Plannig
-        </span>
+        <span className="main__title_text main__title_poker">Poker</span>
+        <p className="main__title_slash"></p>
+        <span className="main__title_text main__title_planning">Plannig</span>
       </div>
-      <div className="static__section">
-        <p className="static__section_text">Start your planning:</p>
-        <div className="static__section_button">
-          <p className="static__section_discription">Create session:</p>
+      <div className="main__section">
+        <p className="main__section_text">Start your planning:</p>
+        <div className="main__section_button">
+          <p className="main__section_discription">Create session:</p>
           <Button type={Buttons.Primary} text="New game" />
         </div>
       </div>
-      <div className="static__section">
-        <p className="static__section_text">OR:</p>
+      <div className="main__section">
+        <p className="main__section_text">OR:</p>
         <Input
           label="Connect to lobby by URL:"
           value={url}
